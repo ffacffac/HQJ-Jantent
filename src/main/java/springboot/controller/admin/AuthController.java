1303bc7ea4811b8c59959b45b169251bc3f0be87
hqj <185138375@qq.com>
2019-02-01 18:03:24 +0800

    提交注释

--- src/main/java/springboot/controller/admin/AuthController.java
+++ src/main/java/springboot/controller/admin/AuthController.java
@@ -29,6 +29,14 @@ import java.io.IOException;
  *
  * @author tangj
  * @date 2018/1/21 14:07
+ * <p>
+ * **：@RestController注解相当于@ResponseBody ＋ @Controller合在一起的作用。
+ * 1) 如果只是使用@RestController注解Controller，则Controller中的方法无法返回jsp页面或者html，
+ * 配置的视图解析器 InternalResourceViewResolver不起作用，返回的内容就是Return 里的内容。
+ * 2) 如果需要返回到指定页面，则需要用 @Controller配合视图解析器InternalResourceViewResolver才行。
+ * 3) 如果需要返回JSON，XML或自定义mediaType内容到页面，则需要在对应的方法上加上@ResponseBody注解。
+ * 1.使用@Controller 注解，在对应的方法上，视图解析器可以解析return 的jsp,html页面，并且跳转到相应页面
+ * 若返回json等内容到页面，则需要加@ResponseBody注解
  */
 @Controller
 @RequestMapping("/admin")
@@ -85,10 +93,5 @@ public class AuthController extends AbstractController {
             e.printStackTrace();
             logger.error("注销失败", e);
         }
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/mylog")
-    public RestResponseBo mylog() {
-        return new RestResponseBo(true);
     }
 }
